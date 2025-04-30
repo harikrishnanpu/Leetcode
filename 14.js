8d5df933cc4cd2ac5e41e0e8e9a4a45fb436c74b
Hari krishnan P U <harikrishnan9a@gmail.com>
2025-04-30 21:42:55 +0530

    logic not found asked gpt fot the logic git add .

---
+++ 14.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+let strs = ["flower","flow","flight"]
+
+var longestCommonPrefix = function(strs) {
+
+    for(let i=0;i < strs.length; i++){
+
+        let currChar = strs[0][i];
+
+        for(let j=1; j < strs.length; j++){
+
+            
+
+
+        }
+
+
+
+    }
+
+
+};
+
+console.log(longestCommonPrefix(strs));
