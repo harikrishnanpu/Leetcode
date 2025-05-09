adea32c5bb50f4243ef3877c43c189f5d0ebf36a
Hari krishnan P U <harikrishnan9a@gmail.com>
2025-05-09 21:18:13 +0530

    some changes

---
+++ 28.js
@@ -0,0 +1,13 @@
+
+var haystack = "sadbutsad" ;
+var needle = "sad";
+
+var strStr = function(haystack, needle) {
+
+    console.log(haystack);
+    console.log(needle);
+    return 0;
+
+};
+
+console.log(strStr()); 
